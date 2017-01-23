6bd15fffdac2c935d0187fa971d6fba32384532b
Karl H <karlhansson95@gmail.com>
2017-01-23 16:16:14 +0100

    Implemented a very basic Game class

--- SuperNautic/SuperNautic_Game/src/Core/Game.cpp
+++ SuperNautic/SuperNautic_Game/src/Core/Game.cpp
@@ -1,34 +1,70 @@
 #include "Game.h"
 
+#include <SFML/OpenGL.hpp>
+
 #include "../Log.h"
 
 Game::Game()
-	: _window(sf::VideoMode(800, 600), "Test window")
+	: _window(sf::VideoMode(800, 600), "Test window", sf::Style::Default, sf::ContextSettings(0U, 0U, 0U, 4U, 0U))
 {
 	LOG("Game is being constructed...");
 
-
+	glEnable(GL_DEPTH_TEST);
 }
 
 Game::~Game()
 {
+	LOG("Game is being destructed...");
 
+	CLOSE_LOG();
 }
 
 void Game::run()
 {
+	sf::Clock clock;
+	sf::Time deltaTime = clock.restart();
 
+
+	while (_window.isOpen())
+	{
+		handleEvents();
+		update(deltaTime.asSeconds());
+		render();
+
+		deltaTime = clock.restart();
+	}
 
 }
 
 void Game::handleEvents()
 {
+	sf::Event event;
+	while (_window.pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case sf::Event::Closed:
+			_window.close();
+			break;
+		case sf::Event::KeyPressed:
+			if (event.key.code == sf::Keyboard::Escape)
+			{
+				_window.close();
+			}
+		}
+	}
+
 }
 
 void Game::update(float dt)
 {
+
 }
 
 void Game::render()
 {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+	_window.display();
 }
--- SuperNautic/SuperNautic_Game/src/main.cpp
+++ SuperNautic/SuperNautic_Game/src/main.cpp
@@ -8,33 +8,14 @@
 #include <crtdbg.h> // Leak-checking
 
 #include "Log.h"
+#include "Core\Game.h"
 
 int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello World", sf::Style::Default);
-
-	LOG("Hello World! ", 2017, ". The Log is working.");
-	LOG_ERROR("This is how errors will look");
-
-	glm::vec3 test(0.4f, 0.4f, 1.0f);
-
-	while (window.isOpen()) 
-	{
-		sf::Event event;
-		while (window.pollEvent(event)) 
-		{
-			if (event.type == sf::Event::Closed) 
-			{
-				window.close();
-			}
-		}
-		window.clear();
-
-		window.display();
-	}
-
-	CLOSE_LOG();
+	Game game;
+	game.run();
+	
 	return 0;
 }
