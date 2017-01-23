43ef7db3beed45afe269fd48ac35147707a3be65
Karl H <karlhansson95@gmail.com>
2017-01-23 15:47:58 +0100

    Added leak-checking

--- SuperNautic/SuperNautic_Game/src/main.cpp
+++ SuperNautic/SuperNautic_Game/src/main.cpp
@@ -5,11 +5,14 @@
 #include <SFML\Graphics.hpp>
 #include <SFML\Window.hpp>
 #include <glm\glm.hpp>
+#include <crtdbg.h> // Leak-checking
 
 #include "Log.h"
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello World", sf::Style::Default);
 
 	LOG("Hello World! ", 2017, ". The Log is working.");
