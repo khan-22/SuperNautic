c31dc2e807dfa45af7f0a7311de5a43cf8c4e8d4
Karl H <karlhansson95@gmail.com>
2017-01-23 17:17:07 +0100

    Made the shell of a model loader

---
+++ SuperNautic/SuperNautic_Game/ModelLoader.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef MODEL_LOADER_H
+#define MODEL_LOADER_H
+
+namespace GFX {
+
+	class ModelLoader
+	{
+	public:
+		ModelLoader();
+		~ModelLoader();
+
+	protected:
+	
+
+
+	private:
+
+	};
+
+
+}
+
+#endif //MODEL_LOADER_H
