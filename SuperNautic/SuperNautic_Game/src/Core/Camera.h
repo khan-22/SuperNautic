ddf17d6b51e242dca8743416d1c6a9e61b5dcfa5
Fritjof <fritjof.cavallin@gmail.com>
2017-01-26 15:52:38 +0100

    Added camera

---
+++ SuperNautic/SuperNautic_Game/src/Core/Camera.h
@@ -0,0 +1,42 @@
+#pragma once
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glm\glm.hpp>
+
+class Camera
+{
+public:
+	Camera(float fov, int viewWidth, int viewHeight
+		, const glm::vec3 & pos = glm::vec3(0.0, 0.0, 0.0)
+		, const glm::vec3 & viewDirection = glm::vec3(0.0, 0.0, 0.0)
+		, const glm::vec3 & up = glm::vec3(0.0, 0.0, 0.0));
+	~Camera();
+	glm::mat4 getView() const;
+	glm::mat4 getPerspective() const;
+	glm::mat4 getVP() const;
+	void setPos(const glm::vec3 & newPos);
+	void setPosAged(const glm::vec3 & newPos);
+	void move(const glm::vec3 & moveVec);
+	void setUp(const glm::vec3 & newUp);
+	void setViewDir(const glm::vec3 & newDir);
+	void setViewDirAged(const glm::vec3 & newDir);
+	void setFOV(float newFOV);
+	void changeFOV(float deltaFOV);
+
+private:
+	Camera();
+	inline void updateView();
+
+	glm::mat4 _perspective;
+	glm::mat4 _view;
+	glm::vec3 _pos;
+	glm::vec3 _up;
+	glm::vec3 _viewDir;
+
+	float _fov;
+	int _viewWidth;
+	int _viewHeight;
+};
+
+#endif // !CAMERA_H
