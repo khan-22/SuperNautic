663b5ad6e785c3525bc0be6db3ce40236cfc7271
Toby <toby@pahlavan.se>
2017-01-26 17:23:17 +0100

    Added VERY basic forward rendering shaders. Just send in position, MVP matrices and a color uniform. That way you can have the level segments be one color and the ship etc be a different color.

---
+++ SuperNautic/SuperNautic_Game/src/GFX/Shaders/forward_vs.glsl
@@ -0,0 +1,28 @@
+//Note: UVs and normals are commented out since this is meant to just be a very basic shader
+//just to get up and running. We'll be switching to deferred rendering asap but they are left
+//in the code just in case we would like to get better shading earlier for whatever reason.
+
+#version 400 core
+
+layout (location = 0) in vec3 pos;
+//layout (location = 1) in vec3 uv; //Supports 3D textures
+//layout (location = 2) in vec3 normal;
+
+uniform mat4 uProjection;
+uniform mat4 uView;
+uniform mat4 uModel;
+
+//out VS_OUT
+//{
+//	vec2 uv;
+//	vec3 normal;
+//} vs_out;
+
+void main()
+{
+	//We do not use the W component of the UVs, can be changed
+	//vs_out.uv = uv.xy; 
+	//vs_normal = normal;
+
+	gl_Position = uProjection * uView * uModel * vec4(pos, 1.0f);
+}
