b6a0ec2a75e6a3a6c4a52ff80fb42c827373c4b2
Karl H <karlhansson95@gmail.com>
2017-01-27 15:39:22 +0100

    Changed strings to const references in shader.

--- SuperNautic/SuperNautic_Game/src/GFX/Shaders/Shader.h
+++ SuperNautic/SuperNautic_Game/src/GFX/Shaders/Shader.h
@@ -19,33 +19,33 @@ namespace GFX
 
 			void bind();
 
-			void setUniform(std::string name, GLfloat data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::vec2& data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::vec3& data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::vec4& data, unsigned int count = 1);
+			void setUniform(const std::string& name, GLfloat data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::vec2& data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::vec3& data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::vec4& data, unsigned int count = 1);
+			
+			void setUniform(const std::string& name, GLint data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::ivec2& data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::ivec3& data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::ivec4& data, unsigned int count = 1);
+			
+			void setUniform(const std::string& name, const glm::mat2& data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::mat3& data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::mat4& data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::mat2x3& data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::mat3x2& data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::mat2x4& data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::mat4x2& data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::mat3x4& data, unsigned int count = 1);
+			void setUniform(const std::string& name, const glm::mat4x3& data, unsigned int count = 1);
 
-			void setUniform(std::string name, GLint data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::ivec2& data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::ivec3& data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::ivec4& data, unsigned int count = 1);
-
-			void setUniform(std::string name, const glm::mat2& data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::mat3& data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::mat4& data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::mat2x3& data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::mat3x2& data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::mat2x4& data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::mat4x2& data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::mat3x4& data, unsigned int count = 1);
-			void setUniform(std::string name, const glm::mat4x3& data, unsigned int count = 1);
-
-			void setSampler(std::string name, GLint unit);
+			void setSampler(const std::string& name, GLint unit);
 
 		private:
 			GLuint							_shaderProgram;
 			std::map<std::string, GLuint>	_uniformMap;
 
-			GLuint getUniform(std::string name);
+			GLuint getUniform(const std::string& name);
 	};
 }
 
