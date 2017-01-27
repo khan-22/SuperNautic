40267cd3ee83d5eacac0c76cd9041ac4d9b62499
Mikael Hernvall <mikael.hernvall@gmail.com>
2017-01-27 10:26:23 +0100

    Added GUI example.
    Showcasing GuiContainer and GuiButton

--- SuperNautic/SuperNautic_Game/src/Core/PlayApplicationState.hpp
+++ SuperNautic/SuperNautic_Game/src/Core/PlayApplicationState.hpp
@@ -4,6 +4,7 @@
 #define PLAY_APPLICATION_STATE_HPP
 
 #include "ApplicationState.hpp"
+#include "GuiContainer.hpp"
 
 class PlayApplicationState : public ApplicationState
 {
@@ -13,6 +14,9 @@ public:
     bool bRender() override;
     bool bUpdate(float dtSeconds) override;
     bool bHandleEvent(const sf::Event& event) override;
+
+private:
+    GuiContainer _guiContainer;
 };
 
 
