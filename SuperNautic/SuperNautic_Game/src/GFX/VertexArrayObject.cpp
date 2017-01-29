6ceaaf7eb5abc79d509e91f3e18edf7142815d98
Karl <khansson.nr2@gmail.com>
2017-01-29 23:59:40 +0100

    Started wrapping "VertexArrayObject"

---
+++ SuperNautic/SuperNautic_Game/src/GFX/VertexArrayObject.cpp
@@ -0,0 +1,50 @@
+﻿#include "VertexArrayObject.hpp"
+
+using namespace GFX;
+
+//VertexArrayObject::VertexArrayObject()
+//{
+//	glGenVertexArrays(1, &_vao);
+//}
+//
+//VertexArrayObject::~VertexArrayObject()
+//{
+//	glDeleteVertexArrays(1, &_vao);
+//}
+////bool VertexArrayObject::addVertexBuffer()
+////{
+////	bind();
+////
+////	_vertexBuffers.emplace_back();
+////	VertexBuffer& vertexBuffer = _vertexBuffers.back();
+////
+////}
+//
+//void VertexArrayObject::bind() const
+//{
+//	glBindVertexArray(_vao);
+//}
+//
+//void VertexArrayObject::unbind()
+//{
+//	glBindVertexArray(0);
+//}
+//
+///////////////////////////////////////////////////////////////
+////	VertexArrayObject::VertexBuffer
+///////////////////////////////////////////////////////////////
+//
+////VertexArrayObject::VertexBuffer::VertexBuffer(std::vector<>& vertices)
+////{
+////	glGenBuffers(1, &_vbo);
+////	glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+////	glBufferData(GL_ARRAY_BUFFER, )
+////	
+////
+////}
+//
+//VertexArrayObject::VertexBuffer::~VertexBuffer()
+//{
+//}
+
+
