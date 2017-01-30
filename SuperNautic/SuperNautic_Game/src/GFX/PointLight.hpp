0e6005604f4022b116e7a8e00aeff9259b6d214a
Toby <toby@pahlavan.se>
2017-01-30 15:00:38 +0100

    Added GetLightProperties function which returns all of the information about a light, ready for rendering.

--- SuperNautic/SuperNautic_Game/src/GFX/PointLight.hpp
+++ SuperNautic/SuperNautic_Game/src/GFX/PointLight.hpp
@@ -6,35 +6,52 @@
 #include "glm\glm.hpp"
 #include "GL\glew.h"
 
+struct PointLightProperties
+{
+	glm::vec3	Position;
+	glm::vec3	AmbientColor;
+	glm::vec3	DiffuseColor;
+	GLfloat		Intensity;
+
+	bool		bLightActive;
+
+	GLfloat constant;
+	GLfloat linear;
+	GLfloat quadratic;
+};
+
 class PointLight
 {
-	public:
-		PointLight(glm::vec3 Position, glm::vec3 AmbientColor, glm::vec3 DiffuseColor, GLfloat Intensity);
+public:
+	PointLight(glm::vec3 Position, glm::vec3 AmbientColor, glm::vec3 DiffuseColor, GLfloat Intensity);
 
-		void SetPosition(glm::vec3 Position);
-		const glm::vec3 GetPosition();
+	void SetPosition(glm::vec3 Position);
+	const glm::vec3 GetPosition();
 
-		void UpdateColor(glm::vec3 AmbientColor, glm::vec3 DiffuseColor);
+	void UpdateColor(glm::vec3 AmbientColor, glm::vec3 DiffuseColor);
 
-		void ChangeIntensity(GLfloat Intensity);
-		
-		void ToggleLight(bool bLightActive);
+	void ChangeIntensity(GLfloat Intensity);
 
-	private:
-		glm::vec3	_Position;
-		glm::vec3	_AmbientColor;
-		glm::vec3	_DiffuseColor;
-		GLfloat		_Intensity;
+	void ToggleLight(bool bLightActive);
 
-		bool		_bLightActive;
+	//Returns a struct with all of the light's properties
+	const PointLightProperties GetLightProperties();
 
-		//Attenuation properties, should probably expose these to allow modifcation in the future
-		GLfloat constant	= 1.0f;
-		GLfloat linear		= 0.045f;
-		GLfloat quadratic	= 0.0075f;
+	~PointLight();
+private:
+	glm::vec3	_Position;
+	glm::vec3	_AmbientColor;
+	glm::vec3	_DiffuseColor;
+	GLfloat		_Intensity;
 
-		PointLight();
-		~PointLight();
+	bool		_bLightActive;
+
+	//Attenuation properties, should probably expose these to allow modifcation in the future
+	GLfloat _Constant = 1.0f;
+	GLfloat _Linear = 0.045f;
+	GLfloat _Quadratic = 0.0075f;
+
+	PointLight();
 };
 
 #endif
