fc328f3c6d26a86839e87f278f3d126fed064ad5
Mikael Hernvall <mikael.hernvall@gmail.com>
2017-01-30 14:50:29 +0100

    Temperature mechanic

--- SuperNautic/SuperNautic_Game/src/Core/Ship.cpp
+++ SuperNautic/SuperNautic_Game/src/Core/Ship.cpp
@@ -7,6 +7,7 @@ Ship::Ship()
 , _bIsJumping(false)
 , _maxSteerAcceleration(2.f)
 , _maxForwardAcceleration(10.f)
+, _engineTemperature(0.f)
 {
 
 }
@@ -16,12 +17,41 @@ void Ship::render()
     // ...
 }
 
+void Ship::updateEngineTemperature(float dt)
+{
+    // Placeholder.. Should probably not be linear. :-)
+    // tempIncrease(speed) = K * speed + M
+    static const float K = 1.5f;
+    static const float BREAK_EVEN_SPEED = 10.f;
+    static const float M = -K * BREAK_EVEN_SPEED;
+    static const float K_NORMALIZED = K / 100.f;
+    static const float M_NORMALIZED = M / 100.f;
+    _engineTemperature += (_forwardVelocity * K_NORMALIZED - M_NORMALIZED) * dt;
+
+    if(_engineTemperature > 1.f)
+    {
+        _engineTemperature = 1.f;
+    }
+    else if(_engineTemperature < 0.f)
+    {
+        _engineTemperature = 0.f;
+    }
+}
+
 void Ship::update(float dt)
 {
+
+    updateEngineTemperature(dt);
+
     // Need racing track segment info...
     glm::vec3 forward(1.f, 0.f, 0.f);
+    forward *= _forwardVelocity;
+
+
     glm::vec3 left(0.f, 1.f, 0.f);
-    move((forward * _forwardVelocity + left * _steerVelocity) * dt);
+    left *= _steerVelocity;
+
+    move((forward + left) * dt);
 }
 
 void Ship::steer(float magnitude)
