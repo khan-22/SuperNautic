d287a0436e230f43af3162601fbdcc60dc9c64c2
Toby <toby@pahlavan.se>
2017-01-31 11:50:41 +0100

    Added light transition function and made code follow coding standards.

--- SuperNautic/SuperNautic_Game/src/GFX/PointLight.hpp
+++ SuperNautic/SuperNautic_Game/src/GFX/PointLight.hpp
@@ -8,48 +8,56 @@
 
 struct PointLightProperties
 {
-	glm::vec3	Position;
-	glm::vec3	AmbientColor;
-	glm::vec3	DiffuseColor;
-	GLfloat		Intensity;
+	glm::vec3	position;
+	glm::vec3	ambientColor;
+	glm::vec3	diffuseColor;
+	GLfloat		intensity;
 
 	bool		bLightActive;
 
-	GLfloat constant;
-	GLfloat linear;
-	GLfloat quadratic;
+	GLfloat		constant;
+	GLfloat		linear;
+	GLfloat		quadratic;
 };
 
 class PointLight
 {
 public:
-	PointLight(glm::vec3 Position, glm::vec3 AmbientColor, glm::vec3 DiffuseColor, GLfloat Intensity);
+	PointLight(glm::vec3 position, glm::vec3 ambientColor, glm::vec3 diffuseColor, GLfloat intensity);
 
-	void SetPosition(glm::vec3 Position);
-	const glm::vec3 GetPosition();
+	void setPosition(glm::vec3 position);
+	const glm::vec3 getPosition();
+	void updateColor(glm::vec3 ambientColor, glm::vec3 diffuseColor);
+	
+	void changeIntensity(GLfloat intensity);
 
-	void UpdateColor(glm::vec3 AmbientColor, glm::vec3 DiffuseColor);
+	void transitionTo(GLfloat newIntensity, GLfloat transitionTime);
+	void update(GLfloat dt);
 
-	void ChangeIntensity(GLfloat Intensity);
+	void toggleLight(bool bLightActive);
 
-	void ToggleLight(bool bLightActive);
 
 	//Returns a struct with all of the light's properties
-	const PointLightProperties GetLightProperties();
+	const PointLightProperties getLightProperties();
 
 	~PointLight();
 private:
-	glm::vec3	_Position;
-	glm::vec3	_AmbientColor;
-	glm::vec3	_DiffuseColor;
-	GLfloat		_Intensity;
+	glm::vec3	_position;
+	glm::vec3	_ambientColor;
+	glm::vec3	_diffuseColor;
+
+	GLfloat		_currentIntensity;
+	GLfloat		_newIntensity;
+	GLfloat		_previousIntensity;
+	GLfloat		_transitionTimer;
+	GLfloat		_transitionTotalTime;
 
 	bool		_bLightActive;
 
 	//Attenuation properties, should probably expose these to allow modifcation in the future
-	GLfloat _Constant = 1.0f;
-	GLfloat _Linear = 0.045f;
-	GLfloat _Quadratic = 0.0075f;
+	GLfloat _constant	= 1.0f;
+	GLfloat _linear		= 0.045f;
+	GLfloat _quadratic	= 0.0075f;
 
 	PointLight();
 };
