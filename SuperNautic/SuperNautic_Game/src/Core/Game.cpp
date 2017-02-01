6207d4f18aaa18cafd2faefe1039732998f822c0
Joakim <jjerkenhag@gmail.com>
2017-02-01 14:08:20 +0100

    Fixed error with comment.

--- SuperNautic/SuperNautic_Game/src/Core/Game.cpp
+++ SuperNautic/SuperNautic_Game/src/Core/Game.cpp
@@ -54,50 +54,50 @@ bool Game::bInitialize()
 	glEnable(GL_DEPTH_TEST);
 	glCullFace(GL_BACK);
 
-	//// Cached asset loading **DEMO**
-	//RawMeshAsset testRawMesh = RawMeshCache::get("Segments/s01_straight_aa.fbx");
+	// Cached asset loading **DEMO**
+	RawMeshAsset testRawMesh = RawMeshCache::get("Segments/s01_straight_aa.fbx");
 
-	//if (testRawMesh.get() == nullptr)
-	//{
-	//	LOG("Failed to load model... Oopsie poopsie!");
-	//}
-	//else
-	//{
-	//	LOG("The loaded mesh has: ", testRawMesh.get()->meshes[0].vertices.size(), " vertices");
-	//}
+	if (testRawMesh.get() == nullptr)
+	{
+		LOG("Failed to load model... Oopsie poopsie!");
+	}
+	else
+	{
+		LOG("The loaded mesh has: ", testRawMesh.get()->meshes[0].vertices.size(), " vertices");
+	}
 
-	//// Shader loading **DEMO**
-	//GFX::ShaderLoader shaderLoader("./src/GFX/Shaders/");
-	//GFX::Shader* testShader = shaderLoader.loadShader("forward");
+	// Shader loading **DEMO**
+	/*GFX::ShaderLoader shaderLoader("./src/GFX/Shaders/");
+	GFX::Shader* testShader = shaderLoader.loadShader("forward");*/
 
-	//Asset<GFX::Shader> testShader = ShaderCache::get("forward");
+	Asset<GFX::Shader> testShader = ShaderCache::get("forward");
 
-	//if (testShader.get() == 0)
-	//{
-	//	LOG("Failed to load shader... Oopsie poopsie!");
-	//}
-	//else
-	//{
-	//	LOG("The test shader has been loaded!");
-	//}
+	if (testShader.get() == 0)
+	{
+		LOG("Failed to load shader... Oopsie poopsie!");
+	}
+	else
+	{
+		LOG("The test shader has been loaded!");
+	}
 
-	//// Model loading **DEMO**
-	//ModelAsset testModel = ModelCache::get("test2.fbx");
+	// Model loading **DEMO**
+	ModelAsset testModel = ModelCache::get("test2.fbx");
 
-	//if (testModel.get() == nullptr)
-	//{
-	//	LOG("Failed to load model to GPU... :,(((");
-	//}
-	//else
-	//{
-	//	LOG("WOOOOOW!!");
-	//}
+	if (testModel.get() == nullptr)
+	{
+		LOG("Failed to load model to GPU... :,(((");
+	}
+	else
+	{
+		LOG("WOOOOOW!!");
+	}
 
 
-	/*_model = ModelCache::get("test2.fbx");
+	_model = ModelCache::get("test2.fbx");
 	_shader = ShaderCache::get("forward");
 
-	LOG_GL_ERRORS();*/
+	LOG_GL_ERRORS();
 
 
     activateGlContext3d(false);
@@ -176,13 +176,13 @@ void Game::render()
 	glm::mat4 view = glm::lookAt(glm::vec3{ 3.f * sinf(time), 0.f, 3.f * cosf(time) }, glm::vec3{ 0.f, 0.f, 0.f }, glm::vec3{ 0.f, 1.f, 0.f });
 	glm::mat4 projection = glm::perspective(90.f, (float)_window.getSize().x / (float)_window.getSize().y, 0.3f, 100.f);
 
-	//glm::vec4 color(1.f, 0.f, 0.f, 1.f);
+	glm::vec4 color(1.f, 0.f, 0.f, 1.f);
 
-	//_shader.get()->setUniform("uModel", model);
-	//_shader.get()->setUniform("uView", view);
-	//_shader.get()->setUniform("uProjection", projection);
+	_shader.get()->setUniform("uModel", model);
+	_shader.get()->setUniform("uView", view);
+	_shader.get()->setUniform("uProjection", projection);
 
-	//_shader.get()->setUniform("uColor", color);
+	_shader.get()->setUniform("uColor", color);
 
 	_model.get()->render();
 
@@ -195,7 +195,7 @@ void Game::render()
     fps.setFont(*font.get());
     fps.setString("FPS: " + std::to_string(_fps));
     _window.draw(fps);
-//
+
 	_window.display();
 }
 
