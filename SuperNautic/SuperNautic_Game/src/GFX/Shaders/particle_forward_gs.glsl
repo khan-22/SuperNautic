9209bb859b91312c55a82037e6242c9b13b66020
Karl H <karlhansson95@gmail.com>
2017-02-20 17:07:59 +0100

    Fleshed out hte particle shader

--- SuperNautic/SuperNautic_Game/src/GFX/Shaders/particle_forward_gs.glsl
+++ SuperNautic/SuperNautic_Game/src/GFX/Shaders/particle_forward_gs.glsl
@@ -5,13 +5,14 @@ layout(triangle_strip, max_vertices = 4) out;
 
 uniform mat4 uPerspective;
 
+in vec3 particleColor[];
+in float particleSize[];
+
+out vec3 finalParticleColor;
 //createVertex(vec3 h, vec3 v, vec2 uv)
 //{
 //	vec4 point = gl_in[0].gl_Position + h + v;
 //	gl_Position = uPerspective * point;
-
-
-
 //}
 
 
@@ -19,10 +20,13 @@ void main()
 {
 	vec4 point = gl_in[0].gl_Position;
 	vec3 cameraDir = -point.xyz;
-	float particleSize = 1.f;
 
-	vec3 horizontal = normalize(cross(cameraDir, vec3(0.0, 1.0, 0.0)) * particleSize / 2.0;
-	vec3 vertical	= normalize(cross(cameraDir, horizontal) * particleSize / 2.0;
+	float size = particleSize[0];
+
+	vec3 horizontal = normalize(cross(cameraDir, vec3(0.0, 1.0, 0.0))) * size / 2.0;
+	vec3 vertical	= normalize(cross(cameraDir, horizontal)) * size / 2.0;
+	
+	finalParticleColor = particleColor[0];
 
 	gl_Position = uPerspective * (point - horizontal + vertical);
 	EmitVertex();
--- SuperNautic/SuperNautic_Game/src/GFX/Shaders/particle_forward_vs.glsl
+++ SuperNautic/SuperNautic_Game/src/GFX/Shaders/particle_forward_vs.glsl
@@ -1,11 +1,17 @@
 #version 400 core
 
 layout (location = 0) in vec3 pos;
+layout (location = 1) in vec3 color;
+layout (location = 2) in float size;
 
 uniform mat4 uView;
 
+out vec3 particleColor;
+out float particleSize;
 
 void main()
 {
+	particleColor = color;
+	particleSize = size;
 	gl_Position = uView * vec4(pos, 1.0);
 }
