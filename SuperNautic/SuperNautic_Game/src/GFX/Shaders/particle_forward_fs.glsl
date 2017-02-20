a15363bec19c4d384f990e3538abcf611875cd07
Karl H <karlhansson95@gmail.com>
2017-02-20 12:39:42 +0100

    Filled out the basic particle shader

--- SuperNautic/SuperNautic_Game/src/GFX/Shaders/particle_forward_fs.glsl
+++ SuperNautic/SuperNautic_Game/src/GFX/Shaders/particle_forward_fs.glsl
@@ -1,1 +1,8 @@
-#pragma once
+#version 400 core
+
+out vec4 fragColor;
+
+void main()
+{
+	fragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
--- SuperNautic/SuperNautic_Game/src/GFX/Shaders/particle_forward_vs.glsl
+++ SuperNautic/SuperNautic_Game/src/GFX/Shaders/particle_forward_vs.glsl
@@ -1,1 +1,11 @@
-#pragma once
+#version 400 core
+
+layout (location = 0) in vec3 pos;
+
+uniform mat4 uView;
+
+
+void main()
+{
+	gl_Position = uView * vec4(pos, 1.0);
+}
