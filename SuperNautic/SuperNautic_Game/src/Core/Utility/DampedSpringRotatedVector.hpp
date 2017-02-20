4aa0e6ca977992fbec10747886d1d4c02fc4e5ba
timmie <timmie.pettersson@gmail.com>
2017-02-20 09:09:44 +0100

    Removed unused class

--- SuperNautic/SuperNautic_Game/src/Core/Utility/DampedSpringRotatedVector.hpp
+++ SuperNautic/SuperNautic_Game/src/Core/Utility/DampedSpringRotatedVector.hpp
@@ -6,11 +6,11 @@
 #include "glm/glm.hpp"
 #include "Core/Utility/SpringRotatedVector.hpp"
 
-class DampedSpringRotatedVector : public SpringRotatedVector
+class DampedSpringRotatedVector// : public SpringRotatedVector
 {
 public:
 	DampedSpringRotatedVector(const glm::vec3& vector, const glm::vec3& target, const glm::vec3& backupAxis, float springConstant);
-	void update(float dt) override;
+	void update(float dt); //override;
 
 	virtual ~DampedSpringRotatedVector() {};
 
