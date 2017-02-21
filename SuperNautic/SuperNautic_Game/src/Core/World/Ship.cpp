f711df251db738b547cf1d384e2cd842863bf9ce
Joakim <jjerkenhag@gmail.com>
2017-02-21 13:45:18 +0100

    Re-implemented dynamic heat limit

--- SuperNautic/SuperNautic_Game/src/Core/World/Ship.cpp
+++ SuperNautic/SuperNautic_Game/src/Core/World/Ship.cpp
@@ -37,7 +37,10 @@ Ship::Ship()
 		_steerStraighteningForce{ 15.0f },
 		_speedResistance{ 0.005f },
 		_preferredHeight{ 2.0f },
-		_engineCooldown{ 0 }
+		_engineCooldown{ 0 },
+		_engineOverload{ 0 },
+		_engineFlashTime{ 0 },
+		_bEngineFlash{ false }
 {
 	_shipModel = GFX::TexturedModel(ModelCache::get("ship.kmf"), MaterialCache::get("test.mat"));
 	setOrigin(glm::vec3{ 0.0f, 0.1f, 0.0f });
@@ -106,9 +109,20 @@ void Ship::update(float dt)
 	// Update engine temperature
 	_engineTemperature = ((_accelerationFactor + _velocity) / 2);
 
-	if (_engineTemperature > 85)
+	if (_engineTemperature > 80)
 	{
-		_engineCooldown = 5;
+		_engineOverload += (_engineTemperature / 100.f) * dt;
+		if (rand() % 500 + 1 < _engineOverload)
+		{
+			_engineCooldown = _engineOverload * _engineTemperature / 50;
+		}
+	}
+	else
+	{
+		if (_engineOverload > 0)
+		{
+			_engineOverload -= (100 - _engineTemperature) * dt;
+		}
 	}
 
 	// Rotate ship forward towards track forward
