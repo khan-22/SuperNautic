d504cf9b4476c03c8483bd9b55d79f4aa452b95a
Mikael Hernvall <mikael.hernvall@gmail.com>
2017-02-22 16:17:19 +0100

    Finalize public interface and generation
    Only multithreaded generation remains.

--- SuperNautic/SuperNautic_Game/src/Core/Gui/GuiTrackGenerator.cpp
+++ SuperNautic/SuperNautic_Game/src/Core/Gui/GuiTrackGenerator.cpp
@@ -1,19 +1,65 @@
-#include "SFML/Window/Event.hpp"
-#include "SFML/Graphics/RenderTarget.hpp"
-
 #include "Core/Gui/GuiTrackGenerator.hpp"
 
 
 
-
-
-void setSize(float width, float height)
+GuiTrackGenerator::GuiTrackGenerator()
+: _segmentHandler("Segments/segmentinfos3.txt", "Segments/ConnectionTypes.txt")
 {
+    _track = std::unique_ptr<Track>(new Track(&_segmentHandler));
+    setTrackAttributes();
 
+    generate();
 }
 
 
 std::unique_ptr<Track> GuiTrackGenerator::takeTrack()
 {
-    return std::move(_track);
+    std::unique_ptr<Track> ret = std::move(_track);
+    _track = std::unique_ptr<Track>(new Track(&_segmentHandler));
+    setTrackAttributes();
+
+    return std::move(ret);
+}
+
+
+void GuiTrackGenerator::render(GFX::RenderStates& states)
+{
+    _preview.render(states);
+}
+
+void GuiTrackGenerator::update(float dtSeconds)
+{
+    _preview.update(dtSeconds);
+}
+
+void GuiTrackGenerator::generate()
+{
+	_track->startNewTrack();
+	while(!_track->bGenerate());
+	_preview.setTrack(*_track);
+}
+
+void GuiTrackGenerator::setTrackAttributes()
+{
+	_track->setCurviness(_curviness);
+	_track->setSeed(_seed);
+	_track->setLength(_length);
+}
+
+void GuiTrackGenerator::setLength(unsigned int length)
+{
+    _track->setLength(length);
+    generate();
+}
+
+void GuiTrackGenerator::setSeed(unsigned int seed)
+{
+    _track->setSeed(seed);
+    generate();
+}
+
+void GuiTrackGenerator::setCurviness(unsigned int curviness)
+{
+    _track->setCurviness(curviness);
+    generate();
 }
--- SuperNautic/SuperNautic_Game/src/Core/Gui/GuiTrackGenerator.hpp
+++ SuperNautic/SuperNautic_Game/src/Core/Gui/GuiTrackGenerator.hpp
@@ -4,26 +4,33 @@
 #define GUI_TRACK_GENERATOR_HPP
 
 #include "Core/Track/Track.hpp"
+#include "Core/Gui/TrackPreview.hpp"
 
 
-#include "Core/Gui/GuiElement.hpp"
-
-
-
-class GuiTrackGenerator : public GuiElement
+class GuiTrackGenerator : public GFX::Renderable3D
 {
     public:
         GuiTrackGenerator();
 
-        void setSize(float width, float height);
-
-        sf::FloatRect getBoundingRect() const override;
-
         std::unique_ptr<Track> takeTrack();
+        void render(GFX::RenderStates& states) override;
+        void update(float dtSeconds);
+
+        void setLength(unsigned int length);
+        void setSeed(unsigned int seed);
+        void setCurviness(unsigned int curviness);
 
     private:
+        SegmentHandler _segmentHandler;
         std::unique_ptr<Track> _track;
+        TrackPreview _preview;
 
+        unsigned int _length = 40000;
+        unsigned int _seed = 1;
+        unsigned int _curviness = 3;
+
+        void generate();
+        void setTrackAttributes();
 };
 
 #endif //GUI_TRACK_GENERATOR_HPP
