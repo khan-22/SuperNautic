41e1b6346979a4f1c0af136fe456a915180cb77e
Karl H <karlhansson95@gmail.com>
2017-02-23 18:05:44 +0100

    Preparing for FOV changing

--- SuperNautic/SuperNautic_Game/src/Core/World/Player.cpp
+++ SuperNautic/SuperNautic_Game/src/Core/World/Player.cpp
@@ -120,6 +120,10 @@ void Player::update(float dt)
 	_hud.setHeatWhite(_ship.getOverload(dt));
 	_hud.update();
 
+	float speed = _ship.getSpeed();
+	float newFOV = 90.f + 50.f * (speed - 20.f) / 180.f;
+
+	//_currentCamera->setFOV(newFOV);
 	_audio.setPitch(PlayerAudio::Sounds::engine, _ship.getEngineTemperature() / 100 + 1);
 }
 
