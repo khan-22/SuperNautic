5ea63b0bd72812adeb97e79755d0a93b1cce81f7
Karl H <karlhansson95@gmail.com>
2017-03-02 16:19:32 +0100

    Created the zone rendering shader

---
+++ SuperNautic/SuperNautic_Game/src/GFX/Shaders/zone_forward_fs.glsl
@@ -0,0 +1,43 @@
+#version 400 core
+
+out vec4 outColor;
+
+uniform sampler2D uDiffuse;
+//uniform sampler2D uSpecular;
+//uniform sampler2D uNormal;
+
+in VS_OUT
+{
+	vec3 position;
+	vec3 uv;
+	vec3 normal;
+} fs_in;
+
+vec3 lightDir = vec3(-0.2, -0.2, -0.2);
+
+void main()
+{
+	//Just output a basic color
+	lightDir = normalize(lightDir);
+
+
+	vec4 diffuse	= texture(uDiffuse, fs_in.uv.xy);
+	//vec4 specular	= texture(uSpecular, fs_in.uv);
+	//vec4 normal		= texture(uNormal, fs_in.uv);
+	
+	//mat3 fromTriangleSpace = mat3(fs_in.tangent, fs_in.bitangent, fs_in.normal);
+	//vec3 normal = texture(uNormal, fs_in.uv).xyz * 2.f - 1.f;
+	//normal = fromTriangleSpace * normal;
+	
+	//float factor = max(dot(normalize(normal), -lightDir), 0.01);
+
+	int id = int(fs_in.uv.z);
+
+	outColor = vec4(
+		float(id == 0) + 0.5, 
+		float(id == 1) + 0.5, 
+		float(id == 2) + 0.5, 
+		1.0);
+	
+
+}
---
+++ SuperNautic/SuperNautic_Game/src/GFX/Shaders/zone_forward_vs.glsl
@@ -0,0 +1,33 @@
+//Note: UVs and normals are commented out since this is meant to just be a very basic shader
+//just to get up and running. We'll be switching to deferred rendering asap but they are left
+//in the code just in case we would like to get better shading earlier for whatever reason.
+
+#version 400 core
+
+layout (location = 0) in vec3 pos;
+layout (location = 1) in vec3 uv; //Supports 3D textures
+layout (location = 2) in vec3 normal;
+
+//uniform mat4 uProjection;
+//uniform mat4 uView;
+uniform mat4 uModel;
+
+uniform mat4 uMVP;
+
+out VS_OUT
+{
+	vec3 position;
+	vec3 uv;
+	vec3 normal;
+} vs_out;
+
+void main()
+{
+	//We do not use the W component of the UVs, can be changed
+	vs_out.uv	  = vec3(uv.x, 1.0 - uv.y, uv.z); 
+	vs_out.normal = normal;
+
+	vs_out.position = (uModel * vec4(pos, 1.0f)).xyz;
+	gl_Position = uMVP * vec4(pos, 1.0f);
+	
+}
