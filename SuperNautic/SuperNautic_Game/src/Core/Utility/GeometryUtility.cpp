0f906bed8e2a3e6a75de96792dad398ef3695823
Mikael Hernvall <mikael.hernvall@gmail.com>
2017-03-06 08:55:00 +0100

    Add generateBoundingSphere for sphere collection

--- SuperNautic/SuperNautic_Game/src/Core/Utility/GeometryUtility.cpp
+++ SuperNautic/SuperNautic_Game/src/Core/Utility/GeometryUtility.cpp
@@ -1,4 +1,6 @@
 #include <cmath>
+#include <algorithm>
+
 
 #include "Core/Utility/GeometryUtility.hpp"
 
@@ -18,5 +20,33 @@ Sphere generateBoundingSphere(const BoundingBox& obb)
     }
     sphere.radius = std::sqrt(sphere.radius);
 
+    return sphere;
+}
+
+
+
+Sphere generateBoundingSphere(const std::vector<Sphere>& spheres)
+{
+    Sphere sphere;
+    sphere.center = glm::vec3(0.f, 0.f, 0.f);
+    sphere.radius = 0.f;
+    if(spheres.empty())
+    {
+        return sphere;
+    }
+
+    sphere.center = std::accumulate(spheres.begin(), spheres.end(), glm::vec3(0.f, 0.f, 0.f), [](const glm::vec3& sum, const Sphere& s)
+    {
+        return sum + s.center;
+    });
+    sphere.center /= float(spheres.size());
+
+    std::vector<float> distances(spheres.size());
+    std::transform(spheres.begin(), spheres.end(), distances.begin(), [&sphere](const Sphere& s)
+    {
+        return glm::distance(sphere.center, s.center) + s.radius;
+    });
+
+    sphere.radius = *std::max_element(distances.begin(), distances.end());
     return sphere;
 }
