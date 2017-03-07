9605072faf8f6c4b67a3d028cb089a05b2f0a156
Mikael Hernvall <mikael.hernvall@gmail.com>
2017-03-07 14:22:25 +0100

    Pause/resume world on pause state push/pop

--- SuperNautic/SuperNautic_Game/src/Core/ApplicationState/PauseMenuApplicationState.hpp
+++ SuperNautic/SuperNautic_Game/src/Core/ApplicationState/PauseMenuApplicationState.hpp
@@ -11,17 +11,20 @@
 #include "Core/Gui/GuiContainer.hpp"
 #include "Core/Io/Input.hpp"
 #include "Core/Gui/GuiText.hpp"
+class World;
+
 
 class PauseMenuApplicationState : public ApplicationState
 {
 public:
-    PauseMenuApplicationState(ApplicationStateStack& stack, ApplicationContext& context);
+    PauseMenuApplicationState(ApplicationStateStack& stack, ApplicationContext& context, World& world);
 
     void render() override;
     bool bUpdate(float dtSeconds) override;
     bool bHandleEvent(const sf::Event& event) override;
 
 private:
+    World& _world;
     sf::RectangleShape _background;
     GuiContainer _guiContainer;
     Input _input;
