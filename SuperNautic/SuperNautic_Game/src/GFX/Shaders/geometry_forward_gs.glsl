77f84f323483c7bac2c0b19d9308b38f0fd16831
Mikael Hernvall <mikael.hernvall@gmail.com>
2017-03-09 17:11:04 +0100

    Add normal mapping lol

--- SuperNautic/SuperNautic_Game/src/GFX/Shaders/geometry_forward_gs.glsl
+++ SuperNautic/SuperNautic_Game/src/GFX/Shaders/geometry_forward_gs.glsl
@@ -21,31 +21,39 @@ out GS_OUT
 	vec2 uv;
 	vec3 normal;
 	float camToFragDistance;
+	vec3 tangent;
+	vec3 bitangent;
 } gs_out;
 
 float backfaceCull(vec3 normal)
 {
-	float bfacecull = dot(gs_in[0].fragPos - uViewPos, (uModel * vec4(normal, 0.0f)).xyz);
-
-	return bfacecull;
+	return dot(gs_in[0].fragPos - uViewPos, normal);
 }
 
 
 void main()
 {
 	//Calculate normal that we use for backface culling
-	vec3 a = gl_in[0].gl_Position.xyz - gl_in[1].gl_Position.xyz; //Edge 1
-	vec3 b = gl_in[2].gl_Position.xyz - gl_in[1].gl_Position.xyz; //Edge 2
-	vec3 normal = normalize(cross(a, b));
+	vec3 a = gs_in[1].fragPos - gs_in[0].fragPos;
+	vec3 b = gs_in[2].fragPos - gs_in[0].fragPos;
+	vec3 normal = cross(b, a);
 
 	if(!(backfaceCull(normal) < 0.0))
 	{
+		normal = normalize(normal);
+		
+		vec2 deltaUv1 = gs_in[1].uv - gs_in[0].uv;
+		vec2 deltaUv2 = gs_in[2].uv - gs_in[0].uv;
+	
+		gs_out.tangent = normalize(a * deltaUv2.y - b * deltaUv1.y);
+		gs_out.bitangent = normalize(b * deltaUv1.x - a * deltaUv2.x);
 		for (int i = 0; i < 3; i++)
 		{
 			gs_out.uv		= gs_in[i].uv;
 			gs_out.normal	= gs_in[i].normal;
 			gs_out.fragPos	= gs_in[i].fragPos;
 			gs_out.camToFragDistance = distance(gs_out.fragPos, uViewPos);
+	
 			gl_Position		= uMVP * gl_in[i].gl_Position;
 			EmitVertex();
 		}
