3688c81eb326526432dacc2a86fb2cbd4b05f856
Mikael Hernvall <mikael.hernvall@gmail.com>
2018-06-28 22:43:05 +0200

    Remove timer test code

--- SuperNautic/SuperNautic_Game/src/main.cpp
+++ SuperNautic/SuperNautic_Game/src/main.cpp
@@ -11,37 +11,7 @@
 #include "Core/Game.hpp"
 #include "Core/Io/Log.hpp"
 
-#include "Core/Utility/Timer.hpp"
-#include "Core/Utility/TimeSummary.hpp"
-
-void foo(size_t depth)
-{
-  prof::Timer timer("foo");
-  std::cout << rand();
-  if(depth > 9000)
-  {
-    return;
-  }
-  foo(depth + 1);
-}
-
 int main() {
-  {
-    for (size_t i = 0; i < 9000; i++)
-    {
-      prof::Timer timer("rand");
-      std::cout << rand();
-    }
-    std::cout << std::endl;
-
-    foo(0);
-
-
-
-    std::cout << prof::TimeSummary::summarize() << std::endl;
-    getchar();
-    return 0;
-  }
   // _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
   Game game;
 
